39fcc3d53bfede006092b8c3ac82c97bd51c11f9
Yura-Korbut <korbuty@mail.ru>
2018-03-04 23:45:06 +0300

    feature\ solution task

--- src/index.js
+++ src/index.js
@@ -1,26 +1,72 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.data = [initialValue];
+  }
+
+  valueOf() {
+    let arr = this.data,
+        pos;
+
+    while (arr.lastIndexOf('^') >= 0) {
+        pos = arr.lastIndexOf('^');      
+        arr.splice((pos-1), 3, Math.pow(arr[pos-1],arr[pos+1]));
+    }
+
+    while (arr.indexOf('/') >= 0) {
+        pos = arr.indexOf('/');
+        arr.splice((pos-1),3, arr[pos-1]/arr[pos+1])
+    }
+
+    while (arr.indexOf('*') >= 0) {
+      pos =  arr.indexOf('*')
+      arr.splice((pos-1),3, arr[pos-1]*arr[pos+1])
+    }
+
+    while (arr.indexOf('+') >= 0) {
+        pos =  arr.indexOf('+')
+        if (arr[pos-2]=="-") {
+          arr.splice((pos-1), 3, +arr[pos-1] - +arr[pos+1]);
+        }
+        else {
+          arr.splice((pos-1), 3, +arr[pos-1] + +arr[pos+1]);
+        }
+    }
+
+    while (arr.indexOf('-') >= 0) {
+        pos =  arr.indexOf('-') 
+        if (arr[pos-2]=="-") {
+          arr.splice((pos-1), 3, +arr[pos-1] + +arr[pos+1]);
+        }
+        else {
+          arr.splice((pos-1), 3, +arr[pos-1] - +arr[pos+1]);
+        }
+      }
+    return arr[0];
   }
 
   add(number) {
-    // your implementation
+    this.data = this.data.concat(['+', number]);
+   return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.data = this.data.concat(['-', number]);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.data = this.data.concat(['*', number]);
+   return this;
   }
 
   devide(number) {
-    // your implementation
+    this.data = this.data.concat(['/', number]);
+   return this;
   }
 
   pow(number) {
-    // your implementation
+    this.data = this.data.concat(['^', number]);
+    return this;
   }
 }
 
